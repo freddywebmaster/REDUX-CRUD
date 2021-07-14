d1623b5581bafa8b8df66102779761ebfa14ae11
Freddy <freddy12gutierrez@gmail.com>
2021-07-14 04:19:38 -0600

    Rutas añadidas y form creado xd

---
+++ src/components/EditarProducto.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const EditarProducto = () => {
+     return ( 
+         <h1>Editar Producto</h1>
+      );
+ }
+  
+ export default EditarProducto;
---
+++ src/components/Productos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Productos = () => {
+    return ( 
+        <h1>Productos xd</h1>
+     );
+}
+ 
+export default Productos;
